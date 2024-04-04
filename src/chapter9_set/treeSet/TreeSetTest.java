5a9bcfd550c91e8cd9ae486537c71f31e39e49ed
Ashes814 <1534661514@qq.com>
2024-04-04 13:01:27 +0800

    implement treeSet with sorted compareTo

--- src/chapter9_set/treeSet/TreeSetTest.java
+++ src/chapter9_set/treeSet/TreeSetTest.java
@@ -1,4 +1,19 @@
 package chapter9_set.treeSet;
+import java.util.*;
 
 public class TreeSetTest {
+    public static void main(String[] args) {
+        Set<Item> parts = new TreeSet<Item>();
+        parts.add(new Item("Modem", 9912));
+        parts.add(new Item("Toaster", 1234));
+        parts.add(new Item("Widget", 4562));
+
+        System.out.println(parts);
+
+        Set<Item> sortByDescription = new TreeSet<Item>(Comparator.comparing(Item::getDescription));
+
+        sortByDescription.addAll(parts);
+        System.out.println(sortByDescription);
+
+    }
 }
